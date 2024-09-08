7d30c9f3a7ffa04cf8bb78761ad885148635b2a7
brebribre <70563428+brebribre@users.noreply.github.com>
2024-09-08 12:59:58 +0200

    style excel

--- data_processing/utils.py
+++ data_processing/utils.py
@@ -14,26 +14,13 @@ def save_processed_data_to_excel(writer, processed_data):
     for sheet_name, data in processed_data.items():
         data.to_excel(writer, sheet_name=sheet_name, index=False)
 
-def apply_styles(output_file):
-    # Load the workbook to apply formatting
-    wb = load_workbook(output_file)
-
-    for sheet_name in wb.sheetnames:
-        ws = wb[sheet_name]
-
-        # Apply custom styles for the header row
+def apply_styles(sheets):
+    for sheet_name, ws in sheets.items():
+        # Apply styling to the header row
         for cell in ws[1]:
-            cell.font = Font(bold=True, size=12)
-            cell.fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")  # Yellow background
-            cell.alignment = Alignment(horizontal="center", vertical="center")
-
-        # Adjust column widths
+            cell.font = Font(bold=True)
+        
         ws.column_dimensions['A'].width = 20  # Nomor Referensi SKU
         ws.column_dimensions['B'].width = 80  # Nama Produk
         ws.column_dimensions['C'].width = 10  # Jumlah
         ws.column_dimensions['D'].width = 20  # Total Harga Produk
-
-    # Save the workbook with the applied styles
-    wb.save(output_file)
-    print(f"Styles applied and file saved as '{output_file}'.")
-
